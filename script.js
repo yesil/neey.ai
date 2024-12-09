20e9f58251a713a43eed9b53b4adfa10881a3643
Ilyas Türkben <ilyas@adobe.com>
2024-12-09 13:23:15 +0100

    update sw

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (error) {
+      console.log("Service Worker registration failed:", error);
+    });
+}
+
 const keyScreen = document.getElementById("keyScreen");
 const mainScreen = document.getElementById("mainScreen");
 const answerScreen = document.getElementById("answerScreen");
